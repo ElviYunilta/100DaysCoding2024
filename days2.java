72aebb2913bd56f6b0f916a2d57d22351c53236b
ElviYunilta <elviyunilta@gmail.com>
2024-09-04 11:01:30 +0800

    Create days2.java

---
+++ days2.java
@@ -0,0 +1,12 @@
+package days3;
+
+public class days3 {
+   public static void main(String[] args) {
+       System.out.println("nama = Elvi Yunilta");
+       System.out.println("alamat = Papalang");
+       System.out.println("TTL = sisango 12 april 2006");
+       System.out.println("jenis kelamin = perempuan");
+       System.out.println("agama = kristen");
+       System.out.println("umur 18 tahun");
+   }
+}
