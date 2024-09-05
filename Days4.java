8609ab8a84751d6041ac47c3aeeb2a6f3217199d
ElviYunilta <elviyunilta@gmail.com>
2024-09-05 18:09:17 +0800

    Create Days4.java

---
+++ Days4.java
@@ -0,0 +1,9 @@
+package days4;
+
+public class days4 {
+    public static void main(String[] args) {
+    System.out.print("Nama saya Elvi yunilta ");
+        System.out.printf("Prodi %s hobby saya %s umur saya %d tahun ","Sistem informasi","Baca buku",18);
+        
+    }
+}
