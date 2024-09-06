7032fd5e4bf36338e3e5791f9b7a0d3e7fe18964
ElviYunilta <elviyunilta@gmail.com>
2024-09-06 14:42:26 +0800

    Create Days5.java

---
+++ Days5.java
@@ -0,0 +1,21 @@
+package days5;
+
+public class days5 {
+    
+    public static void main(String[] args) {
+        String nama="Elvi yunilta";
+        int umur =18;
+        
+        System.out.print("nama :" +nama+ " ");
+        System.out.print("umur :" +umur+ " ");
+        //contoh penggunaan print. yang dimana print menampilkan teks tanpa berpindah baris
+         
+        System.out.println("Saya suka kpop");
+        //contoh penggunaan println. menampilkan teks dan pindah ke garis baru
+        
+        System.out.printf("prodi %s hobby saya %s umur saya %d tahun ","Sistem informasi","Baca buku",18 );
+        //contoh penggunaan printf. menampilkan teks dengan format khusus sesuai placeholder
+        
+    }
+    
+}
