d08aefd6cb86712c3c40391164434aba7142407d
ElviYunilta <elviyunilta@gmail.com>
2024-09-07 17:13:22 +0800

    Create Days6.java

---
+++ Days6.java
@@ -0,0 +1,15 @@
+package days6;
+
+public class days6 {
+    Public static void main(String[] args) {
+        
+        String nama;
+        String hobby;
+        int umur;
+        Double tb;
+        Double bb;
+        
+        
+    }  
+    
+}
