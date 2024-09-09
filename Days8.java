bf9cd35e3256d7cd72268a2e7f1ac6a3f7d8eb3f
ElviYunilta <elviyunilta@gmail.com>
2024-09-09 19:27:10 +0800

    Create Days8.java

---
+++ Days8.java
@@ -0,0 +1,31 @@
+package Day8;
+
+public class Day8 {
+    public static void main(String[] args) {
+        
+        int umur = 18;
+        System.out.println("umur: "+ umur);
+        //sebelum update
+        umur = 82;
+        System.out.println("umur setalah update:"+ umur);
+        
+        String hobby = "volly";
+        System.out.println("hobby: "+ hobby);
+        //sebelum update
+        hobby = "basket";
+        System.out.println("hobby setelah update:"+ hobby);
+        
+        String makanan = "seblak";
+        System.out.println("makanan: "+ makanan);
+        //sebelum update
+        makanan = "bakso";
+        System.out.println("makanan: "+ makanan);
+        
+        
+        
+        
+        
+        
+        
+    }
+}
