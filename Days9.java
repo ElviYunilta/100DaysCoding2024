46a92d89c196c3e9b96e1e2eda9b8d8ac27ff780
ElviYunilta <elviyunilta@gmail.com>
2024-09-10 17:26:39 +0800

    Create Days9.java

---
+++ Days9.java
@@ -0,0 +1,11 @@
+package day9;
+
+public class day9 {
+    public static void main(String[] args) {
+      final  double bilangan = 15.2;
+        System.out.println("bilangan "+ bilangan);
+        
+        
+    }
+    
+}
