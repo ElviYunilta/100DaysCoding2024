816291dc446b1782156d1d8e44488ff477b3fead
ElviYunilta <elviyunilta@gmail.com>
2024-09-12 07:58:31 +0800

    Create days11.java

---
+++ days11.java
@@ -0,0 +1,10 @@
+package days11;
+
+public class days11 {
+    public static void main(String[] args) {
+      short minimal = -32768;
+        short maximal = 32767;
+        System.out.println("nilai minimal type data short adalah "+ minimal +"nilai maximal type data short adalah "+ maximal);
+        
+    }    
+}
