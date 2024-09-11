88a78e45fe8cdf52e66c46a040a8bb19f1bc6d1f
ElviYunilta <elviyunilta@gmail.com>
2024-09-11 18:38:34 +0800

    Create Days10.java

---
+++ Days10.java
@@ -0,0 +1,10 @@
+package days10;
+
+public class days10 {
+    public static void main(String[] args) {
+    byte minimal = -128;
+        byte maximal = 127;
+        System.out.println("nilai minimal tipe data byte "+ minimal +"nilai maximal tipe data byte "+ maximal);
+        
+    } 
+}
