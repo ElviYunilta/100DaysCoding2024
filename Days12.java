50e09b4fec68102282def11f63df5f54b193c3ff
ElviYunilta <elviyunilta@gmail.com>
2024-09-13 12:55:47 +0800

    Create Days12.java

---
+++ Days12.java
@@ -0,0 +1,10 @@
+package days12;
+
+public class days12 {
+    public static void main(String[] args) {
+        int minimal = -2147483648;
+        int maximal = 2147483647;
+        System.out.println("Nilai minimal type data int adalah "+ minimal +"Nilai maximal type int adalah "+ maximal);
+        
+    } 
+}
