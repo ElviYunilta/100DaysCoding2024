2ff85f455f012a8d7923c8fbe9f24116e1e80f32
ElviYunilta <elviyunilta@gmail.com>
2024-09-14 21:43:16 +0800

    C

---
+++ Day13.java
@@ -0,0 +1,32 @@
+package day13;
+
+
+public class Day13 {
+
+    
+    public static void main(String[] args) {
+       final String nama = "elvi yunilta";
+        int umur = 18;
+        int tinggi = 162;
+        //umur sebelum update
+        System.out.println("sebelum di update" ); 
+        System.out.println("nama :"+ nama);
+        System.out.println("umur :"+ umur + "tahun" );
+        System.out.println("tinggi :"+ tinggi + "cm");
+        
+        //tingggi sebelum di update
+        tinggi = 172;
+        umur = 81;
+        System.out.println("setelah di update");
+        System.out.println("nama :"+ nama);
+        System.out.println("umur :"+ umur + "tahun");
+        System.out.println("tinggi:"+ tinggi + "cm");
+         
+        
+        
+        
+        
+       
+    }
+    
+}
