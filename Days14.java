620dd4a5056dafabe6592ed5f32512d53b0dbef2
ElviYunilta <elviyunilta@gmail.com>
2024-09-15 19:16:07 +0800

    Create Days14.java

---
+++ Days14.java
@@ -0,0 +1,11 @@
+package days14;
+
+public class days14 {
+    public static void main(String[] args) {
+        long nilai1 = 12345678;
+        long nilai2 = 3456789;
+        
+        System.out.println("nilai1 type data long adalah "+ nilai1);
+        System.out.println("nilai2 type data long adalah "+ nilai2);
+    }
+}
