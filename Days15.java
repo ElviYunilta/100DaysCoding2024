cafcb715107b1a46f3051cfaf379b0fc2dbd61fb
ElviYunilta <elviyunilta@gmail.com>
2024-09-16 19:36:10 +0800

    Create Days15.java

---
+++ Days15.java
@@ -0,0 +1,10 @@
+package days15;
+
+public class days15 {
+    public static void main(String[] args) {
+        double angka1 = 1234.56;
+        double angka2 = 3456.78;
+        System.out.println("angka1 type data double adalah "+ angka1);
+        System.out.println("angka2 type data double adalah "+ angka2);
+    }
+}
