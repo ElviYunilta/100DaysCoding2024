7c47b5348ce1f2dfb1f5fe5f52950a8b820107db
ElviYunilta <elviyunilta@gmail.com>
2024-09-17 09:45:47 +0800

    Create Days16.java

---
+++ Days16.java
@@ -0,0 +1,14 @@
+package days16;
+
+public class days16 {
+    public static void main(String[] argd) {
+        float e = 123456.789f;
+        float d = 23456.789f;
+        System.out.println("e type data float adalah "+ e);
+        System.out.println("d type data float adalah "+ d);
+        
+        
+        
+    }    
+    
+}
