4b70e01f0d702adda1f592ac03f739b80a9e3d94
ElviYunilta <elviyunilta@gmail.com>
2024-09-18 21:58:14 +0800

    Create Days17.java

---
+++ Days17.java
@@ -0,0 +1,11 @@
+package days17;
+
+public class days17 {
+    public static void main(String[] args) {
+        char a = 'g';
+        char ab = 'c';
+        
+        System.out.println("a type data char adalah "+ a);
+        System.out.println("ab type data char adalah "+ ab);
+    }
+}
