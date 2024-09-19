f6a0faf0d8c68cf17e2b2939ca85337359160194
ElviYunilta <elviyunilta@gmail.com>
2024-09-19 22:14:52 +0800

    Create Days18.java

---
+++ Days18.java
@@ -0,0 +1,13 @@
+package days18;
+
+public class days18 {
+    public static void main (String[] args) {
+        boolean A = false;
+        boolean B = true;
+        System.out.println("A "+ A);
+        System.out.println("B "+ B);
+    
+    }
+    
+}
+    
