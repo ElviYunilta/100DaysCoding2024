fee33ab2f8a1171de4c5428f0546b31906c097cc
ElviYunilta <elviyunilta@gmail.com>
2024-09-20 20:00:15 +0800

    Create Days19.java

---
+++ Days19.java
@@ -0,0 +1,9 @@
+package days19;
+
+public class days19 {
+    public static void main(String[] args) {
+        String namaDepan = "elvy";
+        System.out.println("nama saya :"+ namaDepan);
+    }
+    
+}
