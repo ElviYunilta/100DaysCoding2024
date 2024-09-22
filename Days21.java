7830db10a6f42e14cdda6becbb3255d4ddf090c1
ElviYunilta <elviyunilta@gmail.com>
2024-09-22 21:16:02 +0800

    Create Days21.java

---
+++ Days21.java
@@ -0,0 +1,10 @@
+package days21;
+
+public class days21 {
+    public static void main(String[] args) {
+        Short nilai1 = 123;
+        Short nilai2 = 456;
+        System.out.println("nilai1 :"+ nilai1);
+        System.out.println("nilai2 :"+ nilai2);
+}
+}
