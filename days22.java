62749a6d07271bdf7a5cb8d9cc6551df1cebfb74
ElviYunilta <elviyunilta@gmail.com>
2024-09-23 18:36:33 +0800

    Create days22.java

---
+++ days22.java
@@ -0,0 +1,10 @@
+package days22;
+
+public class days22 {
+    public static void main(String [] args) {
+        Integer aab = 123;
+        System.out.println("aab: "+ aab);
+        
+    }
+        
+}
