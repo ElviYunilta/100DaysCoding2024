ff21611dd9999d45d5ed515ea6ac8a858cfd7693
ElviYunilta <elviyunilta@gmail.com>
2024-09-29 19:38:34 +0800

    Create Days28.java

---
+++ Days28.java
@@ -0,0 +1,10 @@
+package days28;
+
+public class days28 {
+    public static void main(String[] args) {
+        Boolean nilai1 = true;
+        System.out.println("nilai1 "+ nilai1);
+    }
+        
+        
+    }
