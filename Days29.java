7537add2fb529dddcf47ebc971d281fe63a1a19f
ElviYunilta <elviyunilta@gmail.com>
2024-09-30 19:59:42 +0800

    Create Days29.java

---
+++ Days29.java
@@ -0,0 +1,17 @@
+package days29 ;
+import java.math.BigInteger; 
+
+public class days29 {
+    public static void main(String[] args) {
+    
+   
+        
+        BigInteger ab = BigInteger.valueOf(12345678);
+        BigInteger ac = BigInteger.valueOf(234567);
+        BigInteger ad = BigInteger.valueOf(34567);
+        System.out.println(ab);
+        System.out.println(ac);
+        System.out.println(ad);
+    
+}
+}
