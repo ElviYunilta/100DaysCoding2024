5ee185642253b933aa99739a8e1319bbd340cc78
ElviYunilta <elviyunilta@gmail.com>
2024-10-01 21:56:13 +0800

    Create Days30.java

---
+++ Days30.java
@@ -0,0 +1,7 @@
+import java.math.BigDecimal; 
+public class days30 {
+    public static void main(String[] args) {
+        BigDecimal jumlahUang = new BigDecimal ("999.999999");
+        System.out.printf("jumlahUang %f", jumlahUang );
+}
+}
