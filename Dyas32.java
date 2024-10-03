17c25fe6eea31db0a97025a87510bc0fc5dd9c35
ElviYunilta <elviyunilta@gmail.com>
2024-10-03 20:06:35 +0800

    Create Dyas32.java

---
+++ Dyas32.java
@@ -0,0 +1,18 @@
+package Days32;
+
+public class Days32 {
+      public static void main(String[] args) {
+          int isda = 123;
+          String sari = Integer.toString(isda);
+          System.out.println(sari);
+          
+          byte wanty = 24;
+          String via = Byte.toString(wanty);
+          System.out.println(via);
+          
+          short mona = 356;
+          String vebi = Short.toString(mona);
+          System.out.println(vebi);
+          
+}
+}
