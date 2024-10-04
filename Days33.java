5f0347a33ceafb57e97850e7f70b0b0d20725ef4
ElviYunilta <elviyunilta@gmail.com>
2024-10-04 20:32:58 +0800

    Create Days33.java

---
+++ Days33.java
@@ -0,0 +1,11 @@
+package days33;
+
+public class days33 {
+    public static void main(String[] args) {
+        double a = 12.8;
+        System.out.println("nilai double = "+ a);
+        int b = (int) a; //double ke float
+        System.out.println("hasil casting double ke int "+ b);
+    }
+        
+}
