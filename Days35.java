e7f71334b42f61b243b86877f2af9db01561949c
ElviYunilta <elviyunilta@gmail.com>
2024-10-06 22:39:09 +0800

    Create Days35.java

---
+++ Days35.java
@@ -0,0 +1,16 @@
+package days35;
+
+public class days35 {
+    public static void main ( String[] args ){
+        int p;
+        byte q = 8;
+        byte r = 4;
+        //ini adalah penjumlahan
+        p = q+r;
+        System.out.println("hasil penjumlahan adalah "+ p);
+        //pengurangan
+        p = q-r;
+        System.out.println("hasil pengurangan adalah "+ p);
+    }
+        
+}
