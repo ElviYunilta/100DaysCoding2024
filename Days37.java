769a6a9613a91edc9594085d57aeacd7d2d6a6af
ElviYunilta <elviyunilta@gmail.com>
2024-10-08 21:50:29 +0800

    Create Days37.java

---
+++ Days37.java
@@ -0,0 +1,12 @@
+package days37;
+
+public class days37 {
+    public static void main ( String[] args ){
+        double h = 2.5; //mengisi h dengan nilai 2.5
+        h +=1.2;
+        h -=2.1;
+        System.out.println("sisa nilai h: "+ h);
+        
+    }
+        
+}
