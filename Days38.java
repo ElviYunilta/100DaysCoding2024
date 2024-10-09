a5a750e4d7690f7ea35f2d501d035f79dff6b5fc
ElviYunilta <elviyunilta@gmail.com>
2024-10-09 21:35:40 +0800

    Create Days38.java

---
+++ Days38.java
@@ -0,0 +1,14 @@
+package days38;
+
+public class daus38 {
+    public static void main(String[] args) {
+        int k = 20;
+        k /= 4;  // x sekarang bernilai 5
+        System.out.println("Hasil setelah pembagian: "+ k);
+    
+        int g = 10;
+        g *=2; 
+        // g sekarang brnilai 20
+        System.out.println("hasil setelah perkalian: "+ g);
+}
+}
