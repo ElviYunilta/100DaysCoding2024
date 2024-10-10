9186c4c25a2b8d3b4170b25c5146007f1f720cbe
ElviYunilta <elviyunilta@gmail.com>
2024-10-10 22:42:50 +0800

    Create Days39.java

---
+++ Days39.java
@@ -0,0 +1,12 @@
+package days39;
+
+public class days39 {
+    public static void main(String[] args) {
+        int x = 5;
+         int y = 3;  
+          int modulo =x%y;  
+        System.out.println("hasil modulo "+ modulo);
+        
+        
+}
+}
