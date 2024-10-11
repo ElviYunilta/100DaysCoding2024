f6add7f21e70948902ea40b42fe3bece821cb0d7
ElviYunilta <elviyunilta@gmail.com>
2024-10-11 22:23:44 +0800

    Create Days40.java

---
+++ Days40.java
@@ -0,0 +1,17 @@
+package days40;
+
+public class days40 {
+    public static void main(String[] args) {
+        int c = 10;
+        int e = 20;
+        
+        System.out.println(c<e);
+        //c lebih kecil dari e
+        int f = 40;
+        int g = 30;
+        
+        System.out.println(f>g);
+        //f lebih besar dari g
+        
+    }
+}
