c214a5fcbfb4ee3d5b2d19d0b9c0a1072788a4a7
ElviYunilta <elviyunilta@gmail.com>
2024-10-12 22:02:59 +0800

    Create Days41.java

---
+++ Days41.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class day41 {
+    
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in );
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+        int c = a+b;
+        System.out.println(c-b);
+}
+}
