3227808052e83e93866f1925d5bf172c2e101139
ElviYunilta <elviyunilta@gmail.com>
2024-10-13 19:33:21 +0800

    Create Days42.java

---
+++ Days42.java
@@ -0,0 +1,9 @@
+package days42;
+
+public class days42 {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 13;
+        System.out.println("apakah nilai a sama dengan nilai b: "+ (a==b));
+    }
+}
