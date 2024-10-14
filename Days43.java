f447c5dfdcdb93f8639b56fd94e7072cc8e55dcd
ElviYunilta <elviyunilta@gmail.com>
2024-10-14 22:57:57 +0800

    Create Days43.java

---
+++ Days43.java
@@ -0,0 +1,17 @@
+package days43;
+
+public class days43 {
+    
+public static void main(String[] args) {
+  int a = 10;
+    int b = 8;
+    System.out.println("a lebih besar dari b "+ (a > b));
+    System.out.println("a lebih kecil dari b "+ (a<b));
+    System.out.println("a sama dengan 10"+ (a == 10));
+    
+    
+    
+    
+    
+}
+}
