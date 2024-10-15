8ceffa990806b7b995cb9b66f322da40de29d828
ElviYunilta <elviyunilta@gmail.com>
2024-10-15 21:47:41 +0800

    Create Days44.java

---
+++ Days44.java
@@ -0,0 +1,9 @@
+package days44;
+
+public class days44 {
+    public static void main(String[] args) {
+     boolean a = true;
+        boolean b = false;
+        System.out.println(a&&b);
+    }
+}
