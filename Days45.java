a3ee5a6b4669b38dbe1292cbe14f7116c8702b0e
ElviYunilta <elviyunilta@gmail.com>
2024-10-16 20:53:55 +0800

    Create Days45.java

---
+++ Days45.java
@@ -0,0 +1,11 @@
+package days45;
+
+public class daus45 {
+    public static void main ( String[] args ){
+        boolean a = true;
+        boolean b = true;
+        
+        System.out.println("benar atau salah = "+ (a||b));
+        
+    }
+}
