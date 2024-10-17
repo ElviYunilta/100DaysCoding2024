286d66e8671955fe329612de915ff78c248b7f81
ElviYunilta <elviyunilta@gmail.com>
2024-10-17 22:15:52 +0800

    Create days46.java

---
+++ days46.java
@@ -0,0 +1,10 @@
+package days46;
+
+public class days46 {
+
+    public static void main(String[] args) {
+        boolean ab = false;
+        System.out.println("nilai boolean menggunakan negasi adalah: "+ !ab);
+        
+}
+}
