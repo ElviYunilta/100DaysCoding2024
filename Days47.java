ae7d98539b719db5fb82d00d6f72e250ec3a68c3
ElviYunilta <elviyunilta@gmail.com>
2024-10-18 21:28:39 +0800

    Create Days47.java

---
+++ Days47.java
@@ -0,0 +1,10 @@
+public class days47 {
+    public static void main(String[] args) {
+        
+        byte ab = 90;
+        if (ab < 100) {
+        System.out.println("ab lebih kecil dari 100" );
+        
+    }
+    }
+}
