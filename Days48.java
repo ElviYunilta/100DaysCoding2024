9aa6be990e0d271b636cfbb4fa550af26305c8f3
ElviYunilta <elviyunilta@gmail.com>
2024-10-19 22:23:25 +0800

    Create Days48.java

---
+++ Days48.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class days48 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("masukkan nilai: ");
+        int a = input.nextInt();
+        if (a <= 100){
+        System.out.println("nilai lebih besar dari 20: ");
+        } else {
+        System.out.println("nilai lebih kecil dari 60: ");
+        
+    }
+}
+}
