69dd4008082f34a6845973391459ced6c93efc38
ElviYunilta <elviyunilta@gmail.com>
2024-10-21 22:52:35 +0800

    Create Days50.java

---
+++ Days50.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class days50 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai: ");
+        int nilai = scanner.nextInt();
+        String grade;
+
+        if (nilai >= 90) {
+            grade = "A";
+        } else if (nilai >= 70) {
+            grade = "B";
+        } else if (nilai >= 60) {
+            grade = "C";
+        } else if (nilai >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("Grade: " + grade);
+        scanner.close();
+    }
+}
