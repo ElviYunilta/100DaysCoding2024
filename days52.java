b8b36f653f8fe60622a96f19bf22ad0048f96967
ElviYunilta <elviyunilta@gmail.com>
2024-10-23 23:18:45 +0800

    Create days52.java

---
+++ days52.java
@@ -0,0 +1,11 @@
+package days52;
+
+
+public class days52 {
+    public static void main(String[] args) {
+        int nilai =80;
+        String hasil = (nilai >= 75) ? "Lulus":"Tidak Lulus";
+        System.out.println(hasil);
+    }
+    
+}
